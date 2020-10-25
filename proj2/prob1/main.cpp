8882aeccbbd3aeaa84a8c7a8b46f63bdd8160a96
feldblume5263 <feldblume5263@gmail.com>
2020-10-25 15:33:31 +0900

    update code

--- proj2/prob1/main.cpp
+++ proj2/prob1/main.cpp
@@ -8,22 +8,23 @@ using namespace std;
 
 int main()
 {
-	inf_int a, e;
-	inf_int b(100);
-	inf_int c("321111111111122222222222233333333333444444444445555555555");
-	inf_int d("123451987651234572749499923455022211");
-	inf_int f=d;
-	inf_int g(f);
+	inf_int		a, e;
+	inf_int		b(100);
+	inf_int		c("321111111111122222222222233333333333444444444445555555555");
+	inf_int		d("123451987651234572749499923455022211");
+	inf_int		f = d;
+	inf_int		g(f);
 
 	// cin >> g ;   // not required
 
-	a=b*c;
+	a = b * c;
 	// e=g/f;       // not required
 
-	b=c-d;
+	b = c - d;
 
 
-	if (f==d) {
+	if (f == d)
+	{
 		cout << "a : " << a << endl;
 		cout << "b : " << b << endl;
 		cout << "c : " << c << endl;
@@ -33,5 +34,5 @@ int main()
 		// cout << "g : " << g << endl;
 	}
 
-	return 0;
+	return (0);
 }
