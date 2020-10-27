9e1101e02ab2f33a1380508c239e78484a82062a
feldblume5263 <feldblume5263@gmail.com>
2020-10-28 02:23:58 +0900

    update code

--- proj2/main.cpp
+++ proj2/main.cpp
@@ -1,38 +1,10 @@
-#include "inf_int.h"
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <stdlib.h>
+#include "greeter.h"
 
-using namespace std;
-
-int main()
+int				main(void)
 {
-	inf_int		a, e;
-	inf_int		b(100);
-	inf_int		c("321111111111122222222222233333333333444444444445555555555");
-	inf_int		d("123451987651234572749499923455022211");
-	inf_int		f = d;
-	inf_int		g(f);
-
-	// cin >> g ;   // not required
-
-	a = b * c;
-	// e=g/f;       // not required
-
-	b = c - d;
-
-
-	if (f == d)
-	{
-		cout << "a : " << a << endl;
-		cout << "b : " << b << endl;
-		cout << "c : " << c << endl;
-		cout << "d : " << d << endl;
-		// cout << "e : " << e << endl;
-		cout << "f : " << f << endl;
-		// cout << "g : " << g << endl;
-	}
+	Greeter		cal = Greeter();
+	cal.showInputMenu();
 
 	return (0);
 }
